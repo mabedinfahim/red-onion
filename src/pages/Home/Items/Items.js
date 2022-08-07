6dc973373b352461582795fdb57a32560051a327
MINHAJUL ABEDIN <mabedinfahim@gmail.com>
2022-08-07 13:49:15 +0600

    folder structure done

---
+++ src/pages/Home/Items/Items.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Items = () => {
+    return (
+        <div>
+           <h1>This is items page</h1> 
+        </div>
+    );
+};
+
+export default Items;<h1>This is items page</h1>
