2f4bef7c044d2f61477fd15e327bbf7b9301b49b
MINHAJUL ABEDIN <mabedinfahim@gmail.com>
2022-08-08 08:31:31 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import {Routes,Route} from 'react-router-dom'
 import Breakfast from './pages/Breakfast/Breakfast';
 import Home from './pages/Home/Home/Home';
+import Footer from './pages/Shared/Footer/Footer';
+import Header from './pages/Shared/Header/Header'
 
 
 function App() {
   return (
     <>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/home" element={<Home/>}></Route>
@@ -14,6 +17,7 @@ function App() {
         <Route path="/" element={<Home/>}></Route>
         <Route path="/" element={<Home/>}></Route>
       </Routes>
+    <Footer></Footer>
     </>
   );
 }
--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import logo from '../../../assets/images/favicon.png'
 
 const Footer = () => {
+    const date=new Date();
+    const year=date.getFullYear();
     return (
-        <div>
-            <h1>This is footer page</h1>
+        <div className="bg-black px-20 py-10 text-gray-500">
+            <div className='flex justify-between items-center'>
+                <div>
+                    <img className="w-28" src={logo} alt="" srcset="" />
+                </div>
+                <div className='flex justify-between items-center gap-10'>
+                    <ul className="list-none">
+                        <li>About Online food</li>
+                        <li>Read our blog</li>
+                        <li>Sign up to delivery</li>
+                        <li>Add your restaurant</li>
+                    </ul>
+                    <ul className='list-none'>
+                        <li>Get help</li>
+                        <li>Read FQAs</li>
+                        <li>View all cities</li>
+                        <li>Restaurant near me</li>
+                    </ul>
+                </div>
+            </div>
+           <div style={{height: '0.5 px',background:'white'}} className="w-full my-4"></div>
+            <div className='flex justify-between items-center'>
+                <div>Copyright &copy; {year} Red-onion-foods</div>
+                <div>
+                    <ul className="list-none flex justify-between items-center gap-4">
+                        <li>Privacy and Policy</li>
+                        <li>Terms of Use</li>
+                        <li>Pricing</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
