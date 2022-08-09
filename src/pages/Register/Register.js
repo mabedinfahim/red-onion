d11954f9a0b1b23f35383ba90fe5f12fbc117a08
MINHAJUL ABEDIN <mabedinfahim@gmail.com>
2022-08-09 12:04:56 +0600

    nothing to push

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo2.png';
 import auth from '../firebase.init'
@@ -9,11 +9,13 @@ const Register = () => {
     const refPassword=useRef("");
     const refConfirmPass=useRef("")
 
+    const [error,setError]=useState("")
+
     const [
         createUserWithEmailAndPassword,
         user,
         loading,
-        error,
+        error1,
       ] = useCreateUserWithEmailAndPassword(auth);
 
       const [signInWithGoogle, userGoogle, loadingGoogle, errorGoogle] = useSignInWithGoogle(auth);
@@ -28,7 +30,10 @@ const Register = () => {
         const confirmPass=refConfirmPass.current.value;
         console.log(email, password, confirmPass)
         if(password!==confirmPass) {
+            setError("Your password dose not match")
             return;
+        }else{
+            setError("")
         }
 
         console.log("hello")
@@ -45,7 +50,8 @@ const Register = () => {
             <form action="" className="flex flex-col" onSubmit={handelWithSubmit}>
                 <input ref={refEmail} className="border w-80 mb-4 px-4 rounded-md py-1 bg-gray-100" type="email" name="email" placeholder="Email address" id="" />
                 <input ref={refPassword} className='border w-80 mb-4 px-4 rounded-md py-1 bg-gray-100' type="password" name="password" placeholder="Password" id="" />
-                <input ref={refConfirmPass} className='border w-80 mb-4 px-4 rounded-md py-1 bg-gray-100' type="password" name="ConfirmPass" placeholder="Confirm password" id="" />
+                <input ref={refConfirmPass} className='border w-80 mb-1 px-4 rounded-md py-1 bg-gray-100' type="password" name="ConfirmPass" placeholder="Confirm password" id="" />
+                <p className='text-red-500 py-2'>{error}</p>
                 <button type="submit" className="bg-red-500 text-white py-2 rounded-md">Login</button>
                 <div className="flex justify-between items-center mt-4"><div className="w-full h-px bg-gray-500"></div><p className="px-4">Or</p><div className="w-full h-px bg-gray-500"></div></div>
                 <div onClick={()=>signInWithGoogle()} className="py-1 border rounded-md flex justify-center items-center my-2 gap-4"> <img className="w-6 h-6" src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-google-icon-logo-png-transparent-svg-vector-bie-supply-14.png" alt="" srcset="" /><p className="pl-6">Sign With Google</p> </div>
